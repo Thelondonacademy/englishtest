1de181928682e70ed49a57f0c469c51dddd1dc49
Thelondonacademy <info@thelondonacademy.it>
2025-05-27 11:12:28 +0200

    script.js

--- script.js
+++ script.js
@@ -319,7 +319,7 @@ Level: ${qa.level}`;
             accuracy: accuracy,
             wrong_answers: wrongAnswersText,
             suggestions: `• ${suggestions}`,
-            test_date: new Date().toLocaleDateString()
+            test_date: new Date().toLocaleDateString(),
             bcc_email: "test@thelondonacademy.it"
         };
 
